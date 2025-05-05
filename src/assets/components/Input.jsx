66477cfdf29fdac0d25307b0de92e65cd26eba87
YuriSolomon <juristein21@gmail.com>
2025-05-05 12:58:24 +0200

    create components to input new project details

---
+++ src/assets/components/Input.jsx
@@ -0,0 +1,10 @@
+export default function Input({isTextarea, label, ...props}) {
+    return (
+
+        <p>
+            <label>{label}</label>
+            {isTextarea ? <textarea {...props} /> : <input {...props} />}
+        </p>
+
+    )
+}
