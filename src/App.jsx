b5ba117edfc343f184093d80e06cdf388138e5b2
YuriSolomon <juristein21@gmail.com>
2025-05-08 10:34:06 +0200

    add delete project function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Sidebar from "./components/SideBar";
 import SelectedProject from "./components/SelectedProject";
 
 function App() {
-  const [projectsState ,setProjectsState] = useState({selectedProjectId: undefined, projects: []});
+  const [projectsState, setProjectsState] = useState({ selectedProjectId: undefined, projects: [] });
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -44,14 +44,24 @@ function App() {
       return {
         ...prevState,
         selectedProjectId: undefined,
-        projects:[...prevState.projects, newProject]
+        projects: [...prevState.projects, newProject]
       }
     })
   }
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    });
+  }
+
   const selectProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content = <SelectedProject project={selectProject}/>;
+  let content = <SelectedProject project={selectProject} onDelete={handleDeleteProject} />;
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
