dc04509ec5c9254f99541bc855f6f41aa859cf61
YuriSolomon <juristein21@gmail.com>
2025-05-08 11:57:54 +0200

    fix and debug

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -8,13 +8,16 @@ export default function NewTask({ onAdd }) {
     }
 
     function handleClick() {
+        if (enteredTask.trim() === '') {
+            return;
+        }
         onAdd(enteredTask);
         setEnteredTask('');
     }
 
     return (
         <div className="flex items-center gap-4">
-            <input onChange={(event) => handleChange(event)} type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" />
+            <input onChange={(event) => handleChange(event)} type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200" value={enteredTask} />
             <button onClick={handleClick} className="text-stone-700 hover:text-stone-950">Add Task</button>
         </div>
     )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,7 +11,7 @@ export default function Sidebar({ onStartAddProject, projects, onSelectProject, 
                 {projects.map(project => {
                     let cssClasses = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800";
 
-                    if (project.id === selectProjectId) {
+                    if (project === selectProjectId) {
                         cssClasses += ' bg-stone-800 text-stone-200';
                     } else {
                         cssClasses += ' text-stone-400';
